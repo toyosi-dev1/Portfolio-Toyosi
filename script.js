368b2f958c4cea2ebd2a55ec2e267860260ebaf8
Toyosithe1 <149265723+toyosi-dev1@users.noreply.github.com>
2026-09-02 13:23:40 +0100

    Add function to copy email to clipboard

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const revealEls = document.querySelectorAll('.reveal');
     entries.forEach(e => { if(e.isIntersecting){ e.target.classList.add('in-view'); io.unobserve(e.target); } });
   }, { threshold: 0.15 });
   revealEls.forEach(el => io.observe(el));
+
+function copyEmail(event) {
+    const email = "kudiratlawal1000@gmail.com";
+ 
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(email);
+    }
+
+}
